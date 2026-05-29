867c339ae8d0c4585db15ea5be13e27fea202121
aliyoussef2 <52230784@students.liu.edu.lb>
2026-05-29 16:23:57 +0200

    Add migrations, models, seeders, and ReservationController

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ Route::get('/tickets', function () {
     return view('frontend.tickets');
 });Route::get('/reserve', fn() => view('frontend.reserve'))->name('reserve');
 Route::post('/reserve/submit', [App\Http\Controllers\ReservationController::class, 'store'])->name('reserve.store');
+use App\Http\Controllers\ReservationController;
+Route::get('/reserve', [ReservationController::class, 'index'])->name('reserve');
+Route::post('/reserve/submit', [ReservationController::class, 'store'])->name('reserve.store');
