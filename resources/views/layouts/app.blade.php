2b8aa352ccde925baaddb261e075dce3ce80af8a
aliyoussef2 <52230784@students.liu.edu.lb>
2026-05-30 16:00:13 +0200

    Fix layouts app to support both stack and yield styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minieh Fan Zone 2026</title>
+    <title>@yield('title', 'Minieh Fan Zone 2026')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Instrument+Sans:wght@400;600;700&family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
     @stack('styles')
+    @yield('styles')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
